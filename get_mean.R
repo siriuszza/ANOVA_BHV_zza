b3aa5c5974cdbaa153b7ae770d7833a94c713b52
WXYS1209 <xwang2684@wisc.edu>
2024-02-22 15:14:16 -0600

    update mean tree calculation

---
+++ get_mean.R
@@ -0,0 +1,50 @@
+
+work_dir = "./samples_tree"
+
+sturm_mean_jar = "../SturmMean.jar"
+
+mean_trees_dir = "mean_trees"
+
+if (!dir.exists(mean_trees_dir)) {
+  dir.create(mean_trees_dir)
+}
+
+setwd(work_dir)
+
+all_tree_file = "all_tree"
+cat("", file = all_tree_file)
+
+tree_files = list.files(pattern = "\\.txt$")
+
+for (tree in tree_files) {
+  java_cmd = sprintf("java -jar %s -e 0.0001 -u %s",
+                     sturm_mean_jar, tree)
+  
+  mean_tree = system(java_cmd, intern = TRUE)
+  mean_tree_last_line = tail(mean_tree, n = 1)
+  
+  mean_tree_file = file.path("..", mean_trees_dir,
+                             paste0(sub("\\.txt$", "", tree),
+                                    "_mean.txt"))
+  cat("", file = mean_tree_file)
+  writeLines(mean_tree_last_line, mean_tree_file)
+  
+  file.append(all_tree_file, tree)
+}
+
+java_cmd_all = sprintf("java -jar %s -e 0.0001 -u %s",
+                       sturm_mean_jar, all_tree_file)
+mean_all = system(java_cmd_all, intern = TRUE)
+mean_all_last_line = tail(mean_all, n = 1)
+mean_all_filtered = grep("[(].*[)];", 
+                         mean_all_last_line, 
+                         value = TRUE)
+
+mean_all_file = file.path("..", mean_trees_dir, "mean_all.txt")
+writeLines(mean_all_filtered, mean_all_file)
+
+if (file.exists("output.txt")) {
+  file.remove("output.txt")
+}
+
+setwd("../")
--- get_mean.sh
+++ get_mean.sh
@@ -6,14 +6,14 @@ cd samples_tree
 
 > all_tree
 
-for trees in *.txt
+for trees in ./*.txt
 do
-	java -jar SturmMean.jar -e 0.0001 -u $trees |
+	java -jar ../SturmMean.jar -e 0.0001 -u $trees |
 		tail -n 1 > ../mean_trees/${trees%.txt}_mean.txt
 	cat $trees >> all_tree
 done
 
-java -jar SturmMean.jar -e 0.0001 -u all_tree |
-                tail -n 1 > ../mean_trees/mean_all.txt
+java -jar ../SturmMean.jar -e 0.0001 -u all_tree |
+                tail -n 1 | grep -o "(\(.*\));" > ../mean_trees/mean_all.txt
 
 rm output.txt
